4f644ccdcb771b8daaa7c9c93671b7b567feec16
Võ Tường Vi <vi.vo@f4.intek.edu.vn>
2020-11-28 05:09:31 +0700

    Update js to complete upload files function

--- controllers/authController.php
+++ controllers/authController.php
@@ -99,15 +99,15 @@
 
     /** =====================================CLICK POST FORM BUTTON ACTION=========================================== */
 
-    if (isset($_POST["post-form-btn"])){
-        $target_dir = "uploads/";
-        $target_file = $target_dir . $_FILES["fileToUpload"]["name"];
+    // if (isset($_POST["post-form-btn"])){
+    //     $target_dir = "uploads/";
+    //     $target_file = $target_dir . $_FILES["fileToUpload"]["name"];
 
-        if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            die("Sorry, there was an error uploading your file.");
-        }
+    //     if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    //         die("Sorry, there was an error uploading your file.");
+    //     }
 
-    }
+    // }
 
     if(isset($_POST["remove-people-btn"])){
         if(!empty($_POST['check_list'])){
--- main.js
+++ main.js
@@ -28,21 +28,51 @@ $('.post-alter-icon').on('click', function(){
     $('.post-alter-icon ul').toggle();
 });
 $('.add_upload_button_js').on('click',function(){
-    $('.question_poll_item').append("<li class='poll_li_1' style='display:list-item;'><div class='poll-li'><div class='fileinputs'> <input type='file' class='file' name='files[]' multiple><div class='fakefile'> <button type='button' class='button small margin_0'>Select file</button><span> <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-upload' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z' /> <path fill-rule='evenodd' d='M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z' /></svg>Browse</span></div><div class='del-poll-li'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-x-square-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'> <path fill-rule='evenodd' d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z' /></svg> </div> </div> </div></li>");
+    $('li.poll_li_1').css('display', 'list-item');
 });
 
-$('.question_poll_item').on('click', '.del-poll-li', (e) => {
-    $(e.target).parents('li').remove();
+$('.question_poll_item').on('click', '.del-poll-li', () => {
+    $('li.poll_li_1').css('display', 'none');
+    $('.show-files-detail').css('display', 'none');
+    $('#files-detail').text("");
+    $('.file').val(''); 
+    var fi = document.getElementsByClassName('file')[0];
+    $('.fakefile button').text("Total files: "+fi.files.length);
 });
-$('textarea').keypress(function(e){
+$('textarea.comment-input').keypress(function(e){
     var char = e.keyCode ||e.which ;
     if(char == 13){
         $(this).attr('rows', $(this).rows + 1);
         console.log($(this).rows);
     }
 })
-var activeFileInput;
 
-$('body').on('change', 'input[type=file]', function(event){
-    $('.fakefile button').text($(this).val());
+function FileDetails() {
+    var fi = document.getElementsByClassName('file')[0];
+    if (fi.files.length > 0) {
+        $('#files-detail').text(function(){
+            var str = '';
+            for (var i = 0; i <= fi.files.length - 1; i++) {
+
+                var fname = fi.files.item(i).name;      // THE NAME OF THE FILE.
+                var fsize = fi.files.item(i).size;      // THE SIZE OF THE FILE.
+                str += fname + ", ";
+            }
+            return str;
+        });
+        
+    }
+    else { 
+        alert('Please select a file.') 
+    }
+    
+};
+$('body').on('change', 'input[type=file]', function(){
+    var fi = document.getElementsByClassName('file')[0];
+    $('.fakefile button').text("Total files: "+fi.files.length);
+    $('.show-files-detail').css('display', 'block');
+});
+$("#selectAll").click(function(){
+    $(".assignment-visibility input[type=checkbox]").prop('checked', $(this).prop('checked'));
+
 });
